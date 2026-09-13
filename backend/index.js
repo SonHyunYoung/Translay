301136142dd6ab112ca0ecb30b8c8ed3e36fcce9
SonHyunYoung <son2497474@gmail.com>
2026-09-14 03:34:52 +0900

    fix : json 읽어올 수 있게 app.use(express.json()) 추가

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,8 @@ const express = require("express"); // express 모듈 사용 선언
 const port = process.env.PORT;
 const app = express();
 
+app.use(express.json()); //json 읽어올 수 있게 선언
+
 app.use("/api/user", require("./routes/auth")); //회원 관련 API 라우터
 
 app.get("/", (req, res) => { //서버 작동확인
