33c0510db30f0bff39af76fa35330c0ca1cb04a7
SonHyunYoung <son2497474@gmail.com>
2026-09-21 04:30:51 +0900

    feat : 실제 서버 연결 설정

---
+++ app/src/main/java/com/example/transrator/RetrofitClient.kt
@@ -0,0 +1,26 @@
+package com.example.transrator
+
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object RetrofitClient {
+    // 실기기면 본인 PC IP로 변경
+    private const val BASE_URL = "http://222.121.233.244:3000/"
+
+    private val loggingInterceptor = HttpLoggingInterceptor().apply {
+        level = HttpLoggingInterceptor.Level.BODY
+    }
+
+    private val client = OkHttpClient.Builder()
+        .addInterceptor(loggingInterceptor)
+        .build()
+
+    val api: TranslayApi = Retrofit.Builder()
+        .baseUrl(BASE_URL)
+        .client(client)
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+        .create(TranslayApi::class.java)
+}
