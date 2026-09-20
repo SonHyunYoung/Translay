6e7fa57c4548329b6757e57e4f8fb2c4719a2b3b
SonHyunYoung <son2497474@gmail.com>
2026-09-21 01:02:05 +0900

    fix : 인증 미들웨어 exports에서 함수와 이름 안 맞는 것 수정, 토큰 가져올 때 authHeader를 선언하면서 authHeader를 호출하던 부분 해결

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,32 +1,31 @@
-//로그인 인증 
+// 로그인 인증
 require("dotenv").config();
 
 const jwt = require("jsonwebtoken");
 
-const AuthMiddleWare = (req, res, next) => {
+const AuthMiddleware = (req, res, next) => {
+  
+    // 헤더에서 토큰 가져옴
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
 
-    //헤더에서 토큰 가져옴
-    const authHeader = req.headers[authHeader];
-    const token = authHeader && authHeader.split(" ")[1];
+  // 토큰이 존재하지 않을 시 로그인 필요함 안내
+  if (!token) {
+    return res.status(401).json({
+      message: "앱 이용을 위해서는 로그인이 필요합니다."
+    });
+  }
 
-    //토큰이 존재하지 않을 시 로그인 필요함 안내
-    if(!token){
-        return res.status(401).json({
-            message : "앱 이용을 위해서는 로그인이 필요합니다."
-        });
-    }
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET); // 토큰 검증
 
-    try{
-        const decode = jwt.verify(token, process.env.JWT_SECRET); //토큰 검증
+    req.user = decoded;
+    next();
+  } catch (err) {
+    res.status(401).json({
+      message: "유효하지 않은 토큰입니다. 다시 로그인해주세요."
+    });
+  }
+};
 
-        req.user =  decode;
-
-        next();
-    } catch(err){
-        res.status(401).json({
-            message : "유효하지 않은 토큰입니다. 다시 로그인해주세요."
-        });
-    }
-}
-
-module.exports = authMiddleware;
+module.exports = AuthMiddleware;
